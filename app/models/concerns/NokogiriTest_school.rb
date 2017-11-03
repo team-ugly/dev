be436387b45a2c005781b1b715e7b91d93ce9e10
KaedeYamaji <kaede3249@gmail.com>
2017-11-04 00:53:53 +0900

    homeは家で試すようにプロキシの設定を抜いたものになってます schoolは学校で試すようです

--- app/models/concerns/NokogiriTest_school.rb
+++ app/models/concerns/NokogiriTest_school.rb
@@ -1,5 +1,5 @@
 
-class NokogiriTest
+class NokogiriTestSchool
   require 'nokogiri'
   require 'open-uri'
 
@@ -15,19 +15,22 @@ class NokogiriTest
     if '府県天気予報' === entry.xpath('title').text then
       if /.*大分.*/ === entry.xpath('content').text then
         puts "タイトル:" + entry.xpath('title').text
-        puts "link:" + entry.xpath('link').attribute("href").text
+        puts "link:" + entry.xpath('link/@href').text
         puts "観測地点:" + entry.xpath('author/name').text
         puts "コンテンツ:" + entry.xpath('content').text
-        forecast_xml_url = entry.xpath('link').attribute("href").text
+        forecast_xml_url = entry.xpath('link/@href').text
         forecast_xml = Nokogiri::XML(open(forecast_xml_url,prx_opt).read)
         forecast_xml.remove_namespaces!
 
-        detailForecast_nodes = forecast_xml.xpath('//DetailForecast')
-        detailForecast_nodes.each do |a|
-          if '1' === a.xpath('WeatherForecastPart').attribute("refID").text
-            puts "今日の天気予報:" + a.xpath('Sentence').text
+        item_nodes = forecast_xml.xpath('//Item')
+        item_nodes.each do |item|
+          if '中部' === item.xpath('Area/Name').text then
+            weatherForecastPart = item.xpath('//WeatherForecastPart[@refID="1"]')[0]
+            puts forecast_xml.xpath('//TimeDefine[@timeId="1"]')[0].xpath('DateTime').text + ":今日の中部の天気予報:" + weatherForecastPart.xpath('Sentence').text
+            break
           end
         end
+        break
       end
     end
 
