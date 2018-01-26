969e489d093695fc84ab3340a1f3ea3caec8ce4c
hiroki-ishimaru <maru.gooooooo@gmail.com>
2018-01-26 17:14:20 +0000

    yamag

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,7 @@
 
 # Learn more: http://github.com/javan/whenever
 
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, "#{Rails.root}/log/cron.log"
 set :environment, :production
 
