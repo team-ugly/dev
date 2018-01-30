6dd734de174f374b89143227497905215e735aac
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2018-01-31 00:56:36 +0900

    no message

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+  validates :name, presence: true
+  validates :address, presence: true
 end
